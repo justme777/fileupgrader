3a0e9bbfd8976193d60b5f9cbba7b38f56c7d6b2
Abdurakhman <taskibaev.ab@gmail.com>
2015-12-22 19:23:05 +0500

    fileUpgrader - is finished

--- index.php
+++ index.php
@@ -2,32 +2,31 @@
 
 $source_path = $_SERVER["DOCUMENT_ROOT"].'/source';
 
-if(isset($_GET["name"])){
-	$filename = $_GET["name"];
-	$filename = $source_path.'/'.$filename.'.php';
-	if(file_exists($filename)){
-		$text= file_get_contents($filename);
-		$fp = fopen($_SERVER["DOCUMENT_ROOT"].'/temp.txt', 'w');
-		fwrite($fp, $text);
-		fclose($fp);
-		echo "1";	
-	}else{
-		throw new Exception('there is no such php file');
-	}
-}
-
 if(isset($_GET["path"])){
+	$path = $_GET["path"];
 	$filepath = $_GET["path"];
 	if($filepath=="root"){
-		showFileList($source_path);
+		$filepath =$source_path;
 	}else{
 		$filepath=$source_path."/".$filepath;
-		if(is_dir($filepath)){
-			showFileList($filepath);
-		}else{
-			throw new Exception("there is no such directory");
-		}	
 	}
+	
+	if(is_dir($filepath)){
+		showFileList($filepath);
+	}else{
+		if(file_exists($filepath)){
+			$text= file_get_contents($filepath);
+			//$fp = fopen($_SERVER["DOCUMENT_ROOT"].'/temp.txt', 'w');
+			//fwrite($fp, $text);
+			//fclose($fp);
+			echo "<code>";
+			echo $text;
+			echo "</code>";
+			//echo $path;	
+		}else{
+			throw new Exception('there is no such file/directory');
+		}	
+	}	
 	
 }
 
